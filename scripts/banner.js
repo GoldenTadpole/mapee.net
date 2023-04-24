e7928e51f7dc163fe775c55afcdc437634fd22de
GoldenTadpole <122542335+GoldenTadpole@users.noreply.github.com>
2023-04-25 01:54:18 +0300

    Update banner.js

--- scripts/banner.js
+++ scripts/banner.js
@@ -1,3 +1,7 @@
+$(function(){
+    $("#base_page").load("banner_nav.html");
+});
+
 function setUpperVersionLabel(info){
     var upperVersionLabel = document.getElementById("title_version_label");
     upperVersionLabel.innerHTML = "v" + info.version;
